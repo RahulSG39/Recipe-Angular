45f0594a61bce9ae356a0483cccf602105f76523
RahulSG39 <rahulgirish39@gmail.com>
2022-02-20 19:03:16 +0530

    Basic layout no func

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
+
+@Component({
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "First Recipe",
+      "Very nice recipe",
+      "https://www.simplyrecipes.com/thmb/JWjdE8YwikAae0KZuyy6ZJW7Utw=/3000x2001/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg"
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
